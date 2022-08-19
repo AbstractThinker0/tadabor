38e114bcc50b51c56eeead8f51d454faf71e2dc1
hamdi2050 <arabicwriter09@gmail.com>
2022-08-19 22:31:46 +0100

    refactor Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,46 +4,6 @@ function Layout({ children }) {
   return (
     <div className="container vh-100 mw-100">
       <Navbar />
-      {children}
-    </div>
-  );
-}
-
-const Navbar = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            تدبر
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav  mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
-                  الرئيسية
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  حول الموقع
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <div
         className="alert alert-warning alert-dismissible fade show d-flex justify-content-center mt-2"
         role="alert"
@@ -56,7 +16,45 @@ const Navbar = () => {
           aria-label="Close"
         ></button>
       </div>
-    </>
+      {children}
+    </div>
+  );
+}
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          تدبر
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                الرئيسية
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                حول الموقع
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
