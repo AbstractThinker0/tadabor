fc50ca94ae1e76213b317a93bebcd56fda02c1fd
hamdi2050 <arabicwriter09@gmail.com>
2022-09-02 19:37:37 +0100

    optimize RootsBrowser

--- src/components/RootsBrowser.js
+++ src/components/RootsBrowser.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { db } from "../util/db";
@@ -47,8 +47,6 @@ function RootsBrowser() {
     });
   }
 
-  console.log("going to render");
-
   return (
     <div className="pb-3 pt-2">
       <FormWordSearch
@@ -62,77 +60,6 @@ function RootsBrowser() {
     </div>
   );
 }
-
-const RootsListComponent = ({ quranRoots, searchString }) => {
-  const [loadingState, setLoadingState] = useState(true);
-
-  const [myNotes, setMyNotes] = useState({});
-  const [editableNotes, setEditableNotes] = useState({});
-
-  useEffect(() => {
-    let clientLeft = false;
-
-    fetchData();
-
-    async function fetchData() {
-      let userNotes = await db.root_notes.toArray();
-
-      if (clientLeft) return;
-
-      let extractNotes = {};
-      userNotes.forEach((note) => {
-        extractNotes[note.id] = note.text;
-      });
-
-      setMyNotes(extractNotes);
-
-      setLoadingState(false);
-    }
-
-    return () => {
-      clientLeft = true;
-    };
-  }, []);
-
-  if (loadingState) return <LoadingSpinner />;
-
-  return (
-    <div>
-      {quranRoots.current
-        .filter((root) => root.name.startsWith(searchString) || !searchString)
-        .map((root, index) => (
-          <div key={index} className="text-center border">
-            <button
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target={"#collapseExample" + root.id}
-              aria-expanded="false"
-              aria-controls={"collapseExample" + root.id}
-              className="btn"
-              value={root.id}
-            >
-              {root.name}
-            </button>
-            <div
-              className="collapse card border-primary"
-              id={"collapseExample" + root.id}
-            >
-              <div className="card-body">
-                <NoteForm
-                  root={root}
-                  value={myNotes[root.id] || ""}
-                  myNotes={myNotes}
-                  setMyNotes={setMyNotes}
-                  editableNotes={editableNotes}
-                  setEditableNotes={setEditableNotes}
-                />
-              </div>
-            </div>
-          </div>
-        ))}
-    </div>
-  );
-};
 
 const FormWordSearch = ({
   handleSearchSubmit,
@@ -174,90 +101,238 @@ const FormWordSearch = ({
   );
 };
 
-const NoteForm = memo(
-  ({ root, value, myNotes, setMyNotes, editableNotes, setEditableNotes }) => {
-    const [rows, setRows] = useState(4);
+const RootsListComponent = ({ quranRoots, searchString }) => {
+  const [loadingState, setLoadingState] = useState(true);
 
-    let root_id = root.id;
+  const [myNotes, setMyNotes] = useState({});
+  const [editableNotes, setEditableNotes] = useState({});
 
-    if (!value) {
-      editableNotes[root_id] = true;
+  useEffect(() => {
+    let clientLeft = false;
+
+    fetchData();
+
+    async function fetchData() {
+      let userNotes = await db.root_notes.toArray();
+
+      if (clientLeft) return;
+
+      let markedNotes = {};
+
+      let extractNotes = {};
+      userNotes.forEach((note) => {
+        extractNotes[note.id] = note.text;
+        markedNotes[note.id] = false;
+      });
+
+      setMyNotes(extractNotes);
+      setEditableNotes(markedNotes);
+
+      setLoadingState(false);
     }
 
-    let isNoteEditable = editableNotes[root_id];
-
-    useEffect(() => {
-      const rowlen = value.split("\n");
-
-      if (rowlen.length >= 4) {
-        setRows(rowlen.length + 1);
-      } else {
-        setRows(4);
-      }
-    }, [value]);
-
-    const handleNoteChange = (event) => {
-      const { name, value } = event.target;
-      myNotes[name] = value;
-      setMyNotes({ ...myNotes });
+    return () => {
+      clientLeft = true;
     };
+  }, []);
 
-    const handleNoteSubmit = (event) => {
-      event.preventDefault();
+  const memoHandleNoteChange = useCallback(handleNoteChange, []);
 
-      db.root_notes
-        .put({
-          id: root_id,
-          text: value,
-          date_created: Date.now(),
-          date_modified: Date.now(),
-        })
-        .then(function (result) {
-          //
-          toast.success("تم الحفظ بنجاح.");
-          editableNotes[root_id] = false;
-          setEditableNotes({ ...editableNotes });
-        })
-        .catch(function (error) {
-          //
-          toast.success("فشلت عملية الحفظ.");
+  function handleNoteChange(event) {
+    const { name, value } = event.target;
+
+    setMyNotes((state) => {
+      return { ...state, [name]: value };
+    });
+  }
+
+  const memoHandleNoteSubmit = useCallback(handleNoteSubmit, []);
+
+  function handleNoteSubmit(event, value) {
+    event.preventDefault();
+
+    let root_id = event.target.name;
+
+    db.root_notes
+      .put({
+        id: root_id,
+        text: value,
+        date_created: Date.now(),
+        date_modified: Date.now(),
+      })
+      .then(function (result) {
+        //
+        toast.success("تم الحفظ بنجاح.");
+        setEditableNotes((state) => {
+          return { ...state, [root_id]: false };
         });
-    };
+      })
+      .catch(function (error) {
+        //
+        toast.success("فشلت عملية الحفظ.");
+      });
+  }
 
-    const handleEditClick = () => {
-      editableNotes[root_id] = true;
-      setEditableNotes({ ...editableNotes });
-    };
+  const memoHandleEditClick = useCallback(handleEditClick, []);
 
-    return isNoteEditable ? (
-      <form onSubmit={handleNoteSubmit}>
-        <div className="form-group">
-          <textarea
-            className="form-control mb-2"
-            id="textInput"
-            placeholder="أدخل كتاباتك"
-            name={root.id}
-            value={value}
-            onChange={handleNoteChange}
-            rows={rows}
-            required
+  function handleEditClick(event) {
+    let root_id = event.target.name;
+    setEditableNotes((state) => {
+      return { ...state, [root_id]: true };
+    });
+  }
+
+  if (loadingState) return <LoadingSpinner />;
+
+  return (
+    <div>
+      {quranRoots.current
+        .filter((root) => root.name.startsWith(searchString) || !searchString)
+        .map((root) => (
+          <RootComponent
+            key={root.id}
+            root_name={root.name}
+            root_id={root.id}
+            value={myNotes[root.id] || ""}
+            isEditable={editableNotes[root.id]}
+            handleEditClick={memoHandleEditClick}
+            handleNoteSubmit={memoHandleNoteSubmit}
+            handleNoteChange={memoHandleNoteChange}
           />
-        </div>
-        <input type="submit" value="حفظ" className="btn btn-success btn-sm" />
-      </form>
-    ) : (
-      <>
-        <NoteTextComponent>{value}</NoteTextComponent>
-        <button onClick={handleEditClick} className="btn btn-primary btn-sm">
-          تعديل
-        </button>
-      </>
+        ))}
+    </div>
+  );
+};
+
+const RootComponent = memo(
+  ({
+    root_name,
+    root_id,
+    value,
+    handleNoteChange,
+    handleNoteSubmit,
+    handleEditClick,
+    isEditable,
+  }) => {
+    return (
+      <div className="text-center border">
+        <RootButton root_name={root_name} root_id={root_id} />
+        <RootCollapse
+          root_id={root_id}
+          value={value}
+          isEditable={isEditable}
+          handleNoteChange={handleNoteChange}
+          handleNoteSubmit={handleNoteSubmit}
+          handleEditClick={handleEditClick}
+        />
+      </div>
     );
   }
 );
 
-const NoteTextComponent = (props) => {
-  return <p style={{ whiteSpace: "pre-wrap" }}>{props.children}</p>;
+const RootButton = ({ root_name, root_id }) => {
+  return (
+    <button
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target={"#collapseExample" + root_id}
+      aria-expanded="false"
+      aria-controls={"collapseExample" + root_id}
+      className="btn"
+      value={root_id}
+    >
+      {root_name}
+    </button>
+  );
+};
+
+const RootCollapse = ({
+  root_id,
+  value,
+  isEditable,
+  handleNoteChange,
+  handleNoteSubmit,
+  handleEditClick,
+}) => {
+  return (
+    <div
+      className="collapse card border-primary"
+      id={"collapseExample" + root_id}
+    >
+      <div className="card-body">
+        {isEditable === false ? (
+          <NoteTextComponent
+            handleEditClick={handleEditClick}
+            value={value}
+            root_id={root_id}
+          />
+        ) : (
+          <FromComponent
+            root_id={root_id}
+            value={value}
+            handleNoteChange={handleNoteChange}
+            handleNoteSubmit={handleNoteSubmit}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const FromComponent = ({
+  root_id,
+  value,
+  handleNoteSubmit,
+  handleNoteChange,
+}) => {
+  const [rows, setRows] = useState(4);
+
+  useEffect(() => {
+    const rowlen = value.split("\n");
+
+    if (rowlen.length >= 4) {
+      setRows(rowlen.length + 1);
+    } else {
+      setRows(4);
+    }
+  }, [value]);
+
+  return (
+    <form
+      key={root_id}
+      name={root_id}
+      onSubmit={(event) => handleNoteSubmit(event, value)}
+    >
+      <div className="form-group">
+        <textarea
+          className="form-control mb-2"
+          id="textInput"
+          placeholder="أدخل كتاباتك"
+          name={root_id}
+          value={value}
+          onChange={handleNoteChange}
+          rows={rows}
+          required
+        />
+      </div>
+      <input type="submit" value="حفظ" className="btn btn-success btn-sm" />
+    </form>
+  );
+};
+
+const NoteTextComponent = ({ value, root_id, handleEditClick }) => {
+  return (
+    <div>
+      <p style={{ whiteSpace: "pre-wrap" }}>{value}</p>
+      <button
+        name={root_id}
+        onClick={handleEditClick}
+        className="btn btn-primary btn-sm"
+      >
+        تعديل
+      </button>
+    </div>
+  );
 };
 
 export default RootsBrowser;
