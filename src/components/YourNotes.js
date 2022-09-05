daaa56801d92202cf9281c50341cddcca59e26ac
hamdi2050 <arabicwriter09@gmail.com>
2022-09-05 12:27:52 +0100

    update YourNotes page

--- src/components/YourNotes.js
+++ src/components/YourNotes.js
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
 
 import { db } from "../util/db";
+import axios from "axios";
 
 function YourNotes() {
   const [loadingState, setLoadingState] = useState(true);
+  const [chapterNames, setChapterNames] = useState([]);
   const [myNotes, setMyNotes] = useState({});
 
   useEffect(() => {
@@ -24,6 +26,12 @@ function YourNotes() {
 
       setMyNotes(extractNotes);
 
+      const response = await axios.get("/res/chapters.json");
+
+      if (clientLeft) return;
+
+      setChapterNames(response.data);
+
       setLoadingState(false);
     }
 
@@ -34,15 +42,20 @@ function YourNotes() {
 
   if (loadingState) return <LoadingSpinner />;
 
-  return <YourNotesLoaded myNotes={myNotes} setMyNotes={setMyNotes} />;
+  return <YourNotesLoaded myNotes={myNotes} chapterNames={chapterNames} />;
 }
 
-const YourNotesLoaded = ({ myNotes, setMyNotes }) => {
+const YourNotesLoaded = ({ myNotes, chapterNames }) => {
+  const convertKey = (key) => {
+    let info = key.split("-");
+    return chapterNames[info[0] - 1].name + ":" + info[1];
+  };
+
   return (
     <div className="pt-2 pb-2">
       {Object.keys(myNotes).map((key) => (
         <div key={key} className="card mb-3">
-          <div class="card-header">{key}</div>
+          <div className="card-header">{convertKey(key)}</div>
           <div className="card-body">
             <p style={{ whiteSpace: "pre-wrap" }}>{myNotes[key]}</p>
           </div>
