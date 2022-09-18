21a88afe9a6d06121ea161096c79e732bbfe571d
hamdi2050 <arabicwriter09@gmail.com>
2022-09-18 09:31:59 +0100

    remove unused code

--- src/components/SelectionListRoots.js
+++ src/components/SelectionListRoots.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useRef, useState } from "react";
+import { memo, useMemo, useState } from "react";
 import useQuran from "../context/QuranContext";
 
 const SelectionListRoots = memo(
