6933b3d6110dddb7909a6800c44e5dd45f0c8025
hamdi2050 <arabicwriter09@gmail.com>
2022-09-20 12:33:27 +0100

    update YourNotes.js

--- src/components/YourNotes.js
+++ src/components/YourNotes.js
@@ -113,7 +113,7 @@ function YourNotes() {
   if (loadingState) return <LoadingSpinner />;
 
   return (
-    <div className="p-2">
+    <div className="p-2" style={{ height: "90%", overflowY: "scroll" }}>
       {Object.keys(myNotes).map((key) => (
         <div key={key} className="card mb-3">
           <div className="card-header" dir="rtl">
