4c05289725a045783cb389c6ebeb10b616910b0b
EnlightenCode <arabicwriter09@gmail.com>
2022-11-03 17:50:16 +0100

    remove unused code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 // components takes precedence over default styles.
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 
@@ -21,8 +21,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
