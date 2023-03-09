c3354db69570cf06b45baeaf47615246ce0f92a0
EnlightenCode <arabicwriter09@gmail.com>
2023-03-09 11:00:01 +0100

    styles: move bootstrap import to main.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 import "./index.css";
 import App from "./App";
 
