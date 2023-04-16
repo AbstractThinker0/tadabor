f1423438c204c46534695f9db797ecfbc6eac1a7
EnlightenCode <arabicwriter09@gmail.com>
2023-04-16 12:28:24 +0100

    SelectedVerses.tsx: output a notice when no match is found

--- src/components/Coloring/SelectedVerses.tsx
+++ src/components/Coloring/SelectedVerses.tsx
@@ -19,38 +19,45 @@ function SelectedVerses({
     return allQuranText[Number(info[0]) - 1].verses[Number(info[1]) - 1];
   }
 
+  let selectedVerses = Object.keys(coloredVerses).filter((verseKey) =>
+    Object.keys(selectedColors).includes(coloredVerses[verseKey].colorID)
+  );
+
   return (
     <div>
-      {Object.keys(coloredVerses)
-        .filter((verseKey) =>
-          Object.keys(selectedColors).includes(coloredVerses[verseKey].colorID)
-        )
-        .sort((keyA, KeyB) => {
-          let infoA = keyA.split("-");
-          let infoB = KeyB.split("-");
-          if (Number(infoA[0]) !== Number(infoB[0]))
-            return Number(infoA[0]) - Number(infoB[0]);
-          else return Number(infoA[1]) - Number(infoB[1]);
-        })
-        .map((verseKey) => {
-          let verse = getVerseByKey(verseKey);
-          return (
-            <div
-              className="verse-item"
-              key={verseKey}
-              style={{
-                backgroundColor: coloredVerses[verseKey].colorCode,
-                color: getTextColor(coloredVerses[verseKey].colorCode),
-              }}
-            >
-              {verse.versetext} (
-              {chapterNames[Number(verse.suraid) - 1].name +
-                ":" +
-                verse.verseid}
-              )
-            </div>
-          );
-        })}
+      {selectedVerses.length ? (
+        selectedVerses
+          .sort((keyA, KeyB) => {
+            let infoA = keyA.split("-");
+            let infoB = KeyB.split("-");
+            if (Number(infoA[0]) !== Number(infoB[0]))
+              return Number(infoA[0]) - Number(infoB[0]);
+            else return Number(infoA[1]) - Number(infoB[1]);
+          })
+          .map((verseKey) => {
+            let verse = getVerseByKey(verseKey);
+            return (
+              <div
+                className="verse-item"
+                key={verseKey}
+                style={{
+                  backgroundColor: coloredVerses[verseKey].colorCode,
+                  color: getTextColor(coloredVerses[verseKey].colorCode),
+                }}
+              >
+                {verse.versetext} (
+                {chapterNames[Number(verse.suraid) - 1].name +
+                  ":" +
+                  verse.verseid}
+                )
+              </div>
+            );
+          })
+      ) : (
+        <p className="text-center">
+          There are no verses matching the selected colors
+        </p>
+      )}
     </div>
   );
 }
