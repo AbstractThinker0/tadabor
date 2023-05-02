e0f31773600777c01d18a0ed7ee67d2de0a02842
EnlightenCode <arabicwriter09@gmail.com>
2023-05-02 12:59:41 +0100

    Coloring: refactor: split ChaptersSide

---
+++ src/components/Coloring/ChaptersSide.tsx
@@ -0,0 +1,206 @@
+import { useEffect, useRef, Dispatch } from "react";
+import useQuran from "../../context/QuranContext";
+import AddColorModal from "./AddColorModal";
+import DeleteColorModal from "./DeleteColorModal";
+import EditColorsModal from "./EditColorsModal";
+import { clActions, clActionsProps, colorProps, coloredProps } from "./consts";
+import { getTextColor } from "./util";
+import { dbFuncs } from "../../util/db";
+
+interface ChaptersSideProps {
+  currentChapter: number;
+  chapterToken: string;
+  colorsList: coloredProps;
+  dispatchClAction: Dispatch<clActionsProps>;
+  currentColor: colorProps | null;
+  coloredVerses: coloredProps;
+}
+
+function ChaptersSide({
+  currentChapter,
+  chapterToken,
+  colorsList,
+  dispatchClAction,
+  currentColor,
+  coloredVerses,
+}: ChaptersSideProps) {
+  const { chapterNames } = useQuran();
+  const refChapter = useRef<HTMLDivElement | null>(null);
+
+  function onClickChapter(
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    chapterID: number
+  ) {
+    dispatchClAction(clActions.setChapter(chapterID));
+    dispatchClAction(clActions.setChapterToken(""));
+
+    document.documentElement.scrollTop = 0;
+
+    refChapter.current = event.currentTarget;
+  }
+
+  useEffect(() => {
+    if (refChapter.current && refChapter.current.parentElement) {
+      const child = refChapter.current;
+      const parent = refChapter.current.parentElement;
+
+      const parentOffsetTop = parent.offsetTop;
+
+      if (
+        parent.scrollTop + parentOffsetTop <
+          child.offsetTop - parent.clientHeight + child.clientHeight * 2 ||
+        parent.scrollTop + parentOffsetTop >
+          child.offsetTop - child.clientHeight * 1.5
+      ) {
+        parent.scrollTop =
+          child.offsetTop - parentOffsetTop - parent.clientHeight / 2;
+      }
+    }
+  }, [currentChapter]);
+
+  function onChangeChapterToken(event: React.ChangeEvent<HTMLInputElement>) {
+    dispatchClAction(clActions.setChapterToken(event.target.value));
+  }
+
+  function onClickSelectColor(color: colorProps) {
+    dispatchClAction(clActions.selectColor(color));
+  }
+
+  function onClickDeleteColor(color: colorProps) {
+    dispatchClAction(clActions.setCurrentColor(color));
+  }
+
+  function deleteColor(colorID: string) {
+    dispatchClAction(clActions.deleteColor(colorID));
+    dbFuncs.deleteColor(colorID);
+
+    for (const verseKey in coloredVerses) {
+      if (coloredVerses[verseKey].colorID === colorID) {
+        dbFuncs.deleteVerseColor(verseKey);
+      }
+    }
+  }
+
+  function addColor(color: colorProps) {
+    dispatchClAction(clActions.addColor(color));
+  }
+
+  function setColorsList(colorsList: coloredProps) {
+    dispatchClAction(clActions.setColorsList(colorsList));
+
+    Object.keys(colorsList).forEach((colorID) => {
+      dbFuncs.saveColor({
+        id: colorsList[colorID].colorID,
+        name: colorsList[colorID].colorDisplay,
+        code: colorsList[colorID].colorCode,
+      });
+    });
+
+    const newColoredVerses: coloredProps = {};
+    Object.keys(coloredVerses).forEach((verseKey) => {
+      newColoredVerses[verseKey] = colorsList[coloredVerses[verseKey].colorID];
+    });
+
+    dispatchClAction(clActions.setColoredVerses(newColoredVerses));
+  }
+
+  return (
+    <div className="chapters-side">
+      <div className="chapter-block">
+        <input
+          className="chapter-search"
+          type="text"
+          placeholder={chapterNames[currentChapter - 1].name}
+          value={chapterToken}
+          onChange={onChangeChapterToken}
+        />
+        <div className="chapter-list">
+          {chapterNames
+            .filter((chapter) => chapter.name.includes(chapterToken))
+            .map((chapter) => (
+              <div
+                key={chapter.id}
+                onClick={(event) => onClickChapter(event, chapter.id)}
+                className={`chapter-list-item ${
+                  currentChapter === chapter.id
+                    ? "chapter-list-item-selected"
+                    : ""
+                }`}
+              >
+                {chapter.name}
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className="text-center" dir="ltr">
+        Colors list:
+      </div>
+      <div className="chapters-side-colors" dir="ltr">
+        {Object.keys(colorsList).length > 0
+          ? Object.keys(colorsList).map((colorID) => (
+              <div
+                key={colorsList[colorID].colorID}
+                className="chapters-side-colors-item text-center rounded mb-1"
+                style={{
+                  backgroundColor: colorsList[colorID].colorCode,
+                  color: getTextColor(colorsList[colorID].colorCode),
+                }}
+              >
+                <div
+                  onClick={() => onClickSelectColor(colorsList[colorID])}
+                  className="opacity-0"
+                >
+                  🗑️
+                </div>
+                <div
+                  className="flex-grow-1"
+                  onClick={() => onClickSelectColor(colorsList[colorID])}
+                >
+                  {colorsList[colorID].colorDisplay}
+                </div>
+                <div
+                  data-bs-toggle="modal"
+                  data-bs-target="#deleteColorModal"
+                  onClick={() => onClickDeleteColor(colorsList[colorID])}
+                >
+                  🗑️
+                </div>
+              </div>
+            ))
+          : ""}
+      </div>
+      <DeleteColorModal
+        currentColor={currentColor}
+        deleteColor={deleteColor}
+        versesCount={
+          Object.keys(coloredVerses).filter((verseKey) => {
+            return coloredVerses[verseKey]?.colorID === currentColor?.colorID;
+          }).length
+        }
+      />
+      <div className="text-center d-flex gap-2" dir="ltr">
+        <button
+          className="btn btn-dark mt-1"
+          data-bs-toggle="modal"
+          data-bs-target="#colorsModal"
+        >
+          New color
+        </button>
+        <button
+          className="btn btn-info mt-1"
+          data-bs-toggle="modal"
+          data-bs-target="#editColorsModal"
+        >
+          Edit colors
+        </button>
+      </div>
+      <AddColorModal addColor={addColor} />
+      <EditColorsModal
+        colorsList={{ ...colorsList }}
+        setColorsList={setColorsList}
+      />
+    </div>
+  );
+}
+
+export default ChaptersSide;
--- src/pages/Coloring.tsx
+++ src/pages/Coloring.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useReducer, useRef, useState } from "react";
-import useQuran from "../context/QuranContext";
+import { useEffect, useReducer, useState } from "react";
 import { verseProps } from "../types";
 
-import { getTextColor } from "../components/Coloring/util";
 import {
   CL_ACTIONS,
   clActions,
@@ -11,14 +9,11 @@ import {
   coloredProps,
 } from "../components/Coloring/consts";
 
-import AddColorModal from "../components/Coloring/AddColorModal";
-import DeleteColorModal from "../components/Coloring/DeleteColorModal";
-import EditColorsModal from "../components/Coloring/EditColorsModal";
-
 import LoadingSpinner from "../components/LoadingSpinner";
 
 import { IColor, IVerseColor, dbFuncs } from "../util/db";
 import VersesSide from "../components/Coloring/VersesSide";
+import ChaptersSide from "../components/Coloring/ChaptersSide";
 
 interface stateProps {
   currentChapter: number;
@@ -124,7 +119,6 @@ function reducer(state: stateProps, action: clActionsProps): stateProps {
 }
 
 function Coloring() {
-  const { chapterNames } = useQuran();
   const [loadingState, setLoadingState] = useState(true);
 
   useEffect(() => {
@@ -196,194 +190,18 @@ function Coloring() {
 
   const [state, dispatchClAction] = useReducer(reducer, initialState);
 
-  const refChapter = useRef<HTMLDivElement | null>(null);
-
-  function onClickChapter(
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    chapterID: number
-  ) {
-    dispatchClAction(clActions.setChapter(chapterID));
-    dispatchClAction(clActions.setChapterToken(""));
-
-    document.documentElement.scrollTop = 0;
-
-    refChapter.current = event.currentTarget;
-  }
-
-  useEffect(() => {
-    if (refChapter.current && refChapter.current.parentElement) {
-      const child = refChapter.current;
-      const parent = refChapter.current.parentElement;
-
-      const parentOffsetTop = parent.offsetTop;
-
-      if (
-        parent.scrollTop + parentOffsetTop <
-          child.offsetTop - parent.clientHeight + child.clientHeight * 2 ||
-        parent.scrollTop + parentOffsetTop >
-          child.offsetTop - child.clientHeight * 1.5
-      ) {
-        parent.scrollTop =
-          child.offsetTop - parentOffsetTop - parent.clientHeight / 2;
-      }
-    }
-  }, [state.currentChapter]);
-
-  function onChangeChapterToken(event: React.ChangeEvent<HTMLInputElement>) {
-    dispatchClAction(clActions.setChapterToken(event.target.value));
-  }
-
-  function onClickSelectColor(color: colorProps) {
-    dispatchClAction(clActions.selectColor(color));
-  }
-
-  function onClickDeleteColor(color: colorProps) {
-    dispatchClAction(clActions.setCurrentColor(color));
-  }
-
-  function deleteColor(colorID: string) {
-    dispatchClAction(clActions.deleteColor(colorID));
-    dbFuncs.deleteColor(colorID);
-
-    for (const verseKey in state.coloredVerses) {
-      if (state.coloredVerses[verseKey].colorID === colorID) {
-        dbFuncs.deleteVerseColor(verseKey);
-      }
-    }
-  }
-
-  function addColor(color: colorProps) {
-    dispatchClAction(clActions.addColor(color));
-  }
-
-  function setColorsList(colorsList: coloredProps) {
-    dispatchClAction(clActions.setColorsList(colorsList));
-
-    Object.keys(colorsList).forEach((colorID) => {
-      dbFuncs.saveColor({
-        id: colorsList[colorID].colorID,
-        name: colorsList[colorID].colorDisplay,
-        code: colorsList[colorID].colorCode,
-      });
-    });
-
-    const newColoredVerses: coloredProps = {};
-    Object.keys(state.coloredVerses).forEach((verseKey) => {
-      newColoredVerses[verseKey] =
-        colorsList[state.coloredVerses[verseKey].colorID];
-    });
-
-    dispatchClAction(clActions.setColoredVerses(newColoredVerses));
-  }
-
   if (loadingState) return <LoadingSpinner />;
 
   return (
     <div className="coloring">
-      <div className="chapters-side">
-        <div className="chapter-block">
-          <input
-            className="chapter-search"
-            type="text"
-            placeholder={chapterNames[state.currentChapter - 1].name}
-            value={state.chapterToken}
-            onChange={onChangeChapterToken}
-          />
-          <div className="chapter-list">
-            {chapterNames
-              .filter((chapter) => chapter.name.includes(state.chapterToken))
-              .map((chapter) => (
-                <div
-                  key={chapter.id}
-                  onClick={(event) => onClickChapter(event, chapter.id)}
-                  className={`chapter-list-item ${
-                    state.currentChapter === chapter.id
-                      ? "chapter-list-item-selected"
-                      : ""
-                  }`}
-                >
-                  {chapter.name}
-                </div>
-              ))}
-          </div>
-        </div>
-        <div className="text-center" dir="ltr">
-          Colors list:
-        </div>
-        <div className="chapters-side-colors" dir="ltr">
-          {Object.keys(state.colorsList).length > 0
-            ? Object.keys(state.colorsList).map((colorID) => (
-                <div
-                  key={state.colorsList[colorID].colorID}
-                  className="chapters-side-colors-item text-center rounded mb-1"
-                  style={{
-                    backgroundColor: state.colorsList[colorID].colorCode,
-                    color: getTextColor(state.colorsList[colorID].colorCode),
-                  }}
-                >
-                  <div
-                    onClick={() =>
-                      onClickSelectColor(state.colorsList[colorID])
-                    }
-                    className="opacity-0"
-                  >
-                    🗑️
-                  </div>
-                  <div
-                    className="flex-grow-1"
-                    onClick={() =>
-                      onClickSelectColor(state.colorsList[colorID])
-                    }
-                  >
-                    {state.colorsList[colorID].colorDisplay}
-                  </div>
-                  <div
-                    data-bs-toggle="modal"
-                    data-bs-target="#deleteColorModal"
-                    onClick={() =>
-                      onClickDeleteColor(state.colorsList[colorID])
-                    }
-                  >
-                    🗑️
-                  </div>
-                </div>
-              ))
-            : ""}
-        </div>
-        <DeleteColorModal
-          currentColor={state.currentColor}
-          deleteColor={deleteColor}
-          versesCount={
-            Object.keys(state.coloredVerses).filter((verseKey) => {
-              return (
-                state.coloredVerses[verseKey]?.colorID ===
-                state.currentColor?.colorID
-              );
-            }).length
-          }
-        />
-        <div className="text-center d-flex gap-2" dir="ltr">
-          <button
-            className="btn btn-dark mt-1"
-            data-bs-toggle="modal"
-            data-bs-target="#colorsModal"
-          >
-            New color
-          </button>
-          <button
-            className="btn btn-info mt-1"
-            data-bs-toggle="modal"
-            data-bs-target="#editColorsModal"
-          >
-            Edit colors
-          </button>
-        </div>
-        <AddColorModal addColor={addColor} />
-        <EditColorsModal
-          colorsList={{ ...state.colorsList }}
-          setColorsList={setColorsList}
-        />
-      </div>
+      <ChaptersSide
+        chapterToken={state.chapterToken}
+        currentChapter={state.currentChapter}
+        colorsList={state.colorsList}
+        currentColor={state.currentColor}
+        coloredVerses={state.coloredVerses}
+        dispatchClAction={dispatchClAction}
+      />
       <VersesSide
         selectedColors={state.selectedColors}
         coloredVerses={state.coloredVerses}
