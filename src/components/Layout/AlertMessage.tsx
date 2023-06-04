32cf61b4b71df7476f9804a8cb92dbb54291b905
EnlightenCode <arabicwriter09@gmail.com>
2023-06-04 17:05:18 +0100

    AlertMessage.tsx: minor ref

--- src/components/Layout/AlertMessage.tsx
+++ src/components/Layout/AlertMessage.tsx
@@ -2,34 +2,37 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const AlertMessage = () => {
+  const { t } = useTranslation();
+
   const localStorageBetaKey = "betaNotified";
 
   const [betaNotified, setBetaNotified] = useState(
     localStorage.getItem(localStorageBetaKey) !== null
   );
-  const { t } = useTranslation();
 
   function onClickClose() {
     localStorage.setItem(localStorageBetaKey, "true");
     setBetaNotified(true);
   }
 
-  if (betaNotified) return <></>;
-
   return (
-    <div
-      className="alert alert-warning alert-dismissible fade show d-flex justify-content-center m-0"
-      role="alert"
-    >
-      {t("alert_message")}
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-        onClick={onClickClose}
-      ></button>
-    </div>
+    <>
+      {!betaNotified && (
+        <div
+          className="alert alert-warning alert-dismissible fade show d-flex justify-content-center m-0"
+          role="alert"
+        >
+          {t("alert_message")}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+            onClick={onClickClose}
+          ></button>
+        </div>
+      )}
+    </>
   );
 };
 
