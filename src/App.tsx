41370fd01696cfb60aff6ca62788c15f83948e76
EnlightenCode <arabicwriter09@gmail.com>
2023-06-30 21:13:40 +0100

    Lazily load pages components to improve loading time and make use of code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,102 @@
 import { Routes, Route } from "react-router-dom";
 
-import About from "./pages/About";
-import Coloring from "./pages/Coloring";
-import QuranBrowser from "./pages/QuranBrowser";
-import RootsBrowser from "./pages/RootsBrowser";
-import Translation from "./pages/Translation";
-import YourNotes from "./pages/YourNotes";
-import Tags from "./pages/Tags";
-import Inspector from "./pages/Inspector";
-import Comparator from "./pages/Comparator";
+import { lazy, Suspense } from "react";
+
+const About = lazy(() => import("./pages/About"));
+const Coloring = lazy(() => import("./pages/Coloring"));
+const QuranBrowser = lazy(() => import("./pages/QuranBrowser"));
+const RootsBrowser = lazy(() => import("./pages/RootsBrowser"));
+const Translation = lazy(() => import("./pages/Translation"));
+const YourNotes = lazy(() => import("./pages/YourNotes"));
+const Tags = lazy(() => import("./pages/Tags"));
+const Inspector = lazy(() => import("./pages/Inspector"));
+const Comparator = lazy(() => import("./pages/Comparator"));
+
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<QuranBrowser />} />
-      <Route path="/roots" element={<RootsBrowser />} />
-      <Route path="/notes" element={<YourNotes />} />
-      <Route path="/coloring" element={<Coloring />} />
-      <Route path="/translation" element={<Translation />} />
-      <Route path="/tags" element={<Tags />} />
-      <Route path="/inspector" element={<Inspector />} />
-      <Route path="/comparator" element={<Comparator />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<QuranBrowser />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <QuranBrowser />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/roots"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <RootsBrowser />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/notes"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <YourNotes />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/coloring"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Coloring />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/translation"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Translation />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/tags"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Tags />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/inspector"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Inspector />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/comparator"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <Comparator />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <About />
+          </Suspense>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <Suspense fallback={<LoadingSpinner />}>
+            <QuranBrowser />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
