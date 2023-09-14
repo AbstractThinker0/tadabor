09be9b994fb57fe5d31c326b797bf55a1b56a14f
EnlightenCode <arabicwriter09@gmail.com>
2023-09-14 17:58:05 +0100

    Navbar: Minor restyle

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,83 +1,46 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageButton from "./LanguageButton";
 
 const Navbar = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+    <nav>
+      <div className="nav-list">
+        <a className="nav-brand" href="/">
           📖{t("nav_brand")}
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul
-            className={`navbar-nav  mb-2 mb-lg-0 ${
-              i18n.resolvedLanguage === "en" && "me-auto"
-            }`}
-          >
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/">
-                {t("nav_browser")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/roots">
-                {t("nav_roots")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/notes">
-                {t("nav_notes")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/translation">
-                {t("nav_translation")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/coloring">
-                {t("nav_coloring")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tags">
-                {t("nav_tags")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/inspector">
-                {t("nav_inspector")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/comparator">
-                {t("nav_comparator")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                {t("nav_about")}
-              </Link>
-            </li>
-          </ul>
+
+        <div className="nav-list-start">
+          <NavItem to="/" label={t("nav_browser")} />
+          <NavItem to="/roots" label={t("nav_roots")} />
+          <NavItem to="/notes" label={t("nav_notes")} />
+          <NavItem to="/translation" label={t("nav_translation")} />
+          <NavItem to="/coloring" label={t("nav_coloring")} />
+          <NavItem to="/tags" label={t("nav_tags")} />
+          <NavItem to="/inspector" label={t("nav_inspector")} />
+          <NavItem to="/comparator" label={t("nav_comparator")} />
+          <NavItem to="/about" label={t("nav_about")} />
+        </div>
+        <div className="nav-list-end">
           <LanguageButton />
         </div>
       </div>
     </nav>
+  );
+};
+
+interface NavItemProps {
+  label: string;
+  to: string;
+}
+
+const NavItem = ({ label, to }: NavItemProps) => {
+  return (
+    <div className="nav-list-start-item">
+      <NavLink to={to}>{label}</NavLink>
+    </div>
   );
 };
 
