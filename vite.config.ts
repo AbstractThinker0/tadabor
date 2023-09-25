431152504c32dd45b3722a01b64b7efe23992ca1
EnlightenCode <arabicwriter09@gmail.com>
2023-09-25 20:57:54 +0100

    Configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   plugins: [react()],
