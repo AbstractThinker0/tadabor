5e90a667b2f2712a70590305598f60dcf1f1ea2b
EnlightenCode <arabicwriter09@gmail.com>
2023-10-09 10:38:08 +0100

    QuranBrowser: small code ref

--- src/components/QuranBrowser/SelectionListRoots.tsx
+++ src/components/QuranBrowser/SelectionListRoots.tsx
@@ -1,6 +1,7 @@
 import { memo, useMemo, useState, Dispatch } from "react";
-import useQuran from "../../context/QuranContext";
-import { qbActions, qbActionsProps } from "./consts";
+import useQuran from "@/context/QuranContext";
+import { qbActions, qbActionsProps } from "@/components/QuranBrowser/consts";
+import { rootProps } from "@/types";
 
 interface SelectionListRootsProps {
   isDisabled: boolean;
@@ -10,59 +11,13 @@ interface SelectionListRootsProps {
 
 const SelectionListRoots = memo(
   ({ isDisabled, searchString, dispatchQbAction }: SelectionListRootsProps) => {
-    const { quranRoots } = useQuran();
-    const [itemsCount, setItemsCount] = useState(100);
-
-    function handleScroll(event: React.UIEvent<HTMLDivElement>) {
-      const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
-      // Reached the bottom, ( the +1 is needed since the scrollHeight - scrollTop doesn't seem to go to the very bottom for some reason )
-      if (scrollHeight - scrollTop <= clientHeight + 10) {
-        fetchMoreData();
-      }
-    }
-
-    function onClickRoot(rootID: string) {
-      if (isDisabled) return;
-
-      const selectedRoot = quranRoots[Number(rootID)];
-
-      dispatchQbAction(qbActions.setSearchString(selectedRoot.name));
-    }
-
-    const fetchMoreData = () => {
-      setItemsCount((state) => state + 20);
-    };
-
-    const filteredArray = useMemo(
-      () =>
-        quranRoots.filter(
-          (root) => root.name.startsWith(searchString) || isDisabled
-        ),
-      [quranRoots, searchString, isDisabled]
-    );
-
     return (
-      <div className="container browser-search-roots mt-2 p-0">
-        <div
-          className={`browser-search-roots-list ${
-            isDisabled ? "browser-search-roots-list-disabled" : ""
-          }`}
-          onScroll={handleScroll}
-        >
-          {filteredArray.slice(0, itemsCount).map((root) => (
-            <div
-              onClick={() => onClickRoot(root.id.toString())}
-              className={`browser-search-roots-list-item ${
-                searchString === root.name && !isDisabled
-                  ? "browser-search-roots-list-item-selected"
-                  : ""
-              }`}
-              key={root.id}
-            >
-              {root.name}
-            </div>
-          ))}
-        </div>
+      <div className="browser-search-roots">
+        <RootsList
+          isDisabled={isDisabled}
+          searchString={searchString}
+          dispatchQbAction={dispatchQbAction}
+        />
       </div>
     );
   },
@@ -78,5 +33,83 @@ const SelectionListRoots = memo(
 );
 
 SelectionListRoots.displayName = "SelectionListRoots";
+
+interface RootsListProps {
+  isDisabled: boolean;
+  searchString: string;
+  dispatchQbAction: Dispatch<qbActionsProps>;
+}
+
+const RootsList = ({
+  isDisabled,
+  searchString,
+  dispatchQbAction,
+}: RootsListProps) => {
+  const { quranRoots } = useQuran();
+  const [itemsCount, setItemsCount] = useState(50);
+
+  function handleScroll(event: React.UIEvent<HTMLDivElement>) {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
+
+    if (scrollHeight - scrollTop <= clientHeight + 10) {
+      setItemsCount((prevState) => prevState + 20);
+    }
+  }
+
+  const filteredArray = useMemo(
+    () =>
+      quranRoots.filter(
+        (root) => root.name.startsWith(searchString) || isDisabled
+      ),
+    [searchString, isDisabled]
+  );
+
+  function handleRootSelect(rootName: string) {
+    if (isDisabled) return;
+
+    dispatchQbAction(qbActions.setSearchString(rootName));
+  }
+
+  return (
+    <div
+      className={`browser-search-roots-list ${
+        isDisabled ? "browser-search-roots-list-disabled" : ""
+      }`}
+      onScroll={handleScroll}
+    >
+      {filteredArray.slice(0, itemsCount).map((root) => (
+        <RootItem
+          root={root}
+          isSelected={searchString === root.name && !isDisabled}
+          handleRootSelect={handleRootSelect}
+        />
+      ))}
+    </div>
+  );
+};
+
+interface RootItemProps {
+  root: rootProps;
+  isSelected: boolean;
+  handleRootSelect: (rootName: string) => void;
+}
+
+const RootItem = ({ root, isSelected, handleRootSelect }: RootItemProps) => {
+  function onClickRoot(rootName: string) {
+    handleRootSelect(rootName);
+  }
+
+  return (
+    <div
+      onClick={() => onClickRoot(root.name)}
+      className={`browser-search-roots-list-item ${
+        isSelected ? "browser-search-roots-list-item-selected" : ""
+      }`}
+      key={root.id}
+    >
+      {root.name}
+    </div>
+  );
+};
 
 export default SelectionListRoots;
