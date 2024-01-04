80bb157d1ed3f8fe934749e08f4667eb61a57807
EnlightenCode <arabicwriter09@gmail.com>
2024-01-04 17:54:04 +0100

    YourNotes: add translation notes tab

--- src/i18n.ts
+++ src/i18n.ts
@@ -63,6 +63,7 @@ i18n
           search_inclusive: "Inclusive",
           notes_verses: "Verses notes",
           notes_roots: "Roots notes",
+          notes_trans: "Translation",
         },
       },
       ar: {
@@ -113,6 +114,7 @@ i18n
           search_inclusive: "شامل",
           notes_verses: "آيات",
           notes_roots: "جذور",
+          notes_trans: "ترجمة",
         },
       },
     },
--- src/pages/YourNotes.tsx
+++ src/pages/YourNotes.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 
 import RootNotes from "@/components/YourNotes/RootNotes";
 import VerseNotes from "@/components/YourNotes/VerseNotes";
+import TransNotes from "@/components/YourNotes/TransNotes";
 
 function YourNotes() {
   const { t } = useTranslation();
@@ -41,6 +42,20 @@ function YourNotes() {
             {t("notes_roots")}
           </button>
         </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="trans-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#trans-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="trans-tab-pane"
+            aria-selected="false"
+          >
+            {t("notes_trans")}
+          </button>
+        </li>
       </ul>
       <div className="tab-content" id="myTabContent">
         <div
@@ -60,6 +75,15 @@ function YourNotes() {
           tabIndex={0}
         >
           <RootNotes />
+        </div>
+        <div
+          className="tab-pane fade"
+          id="trans-tab-pane"
+          role="tabpanel"
+          aria-labelledby="trans-tab"
+          tabIndex={0}
+        >
+          <TransNotes />
         </div>
       </div>
     </div>
