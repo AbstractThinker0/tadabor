13b73deeba985aa156454d18194233651a928ae2
EnlightenCode <arabicwriter09@gmail.com>
2024-01-19 21:41:51 +0100

    Add the proto of notes backup feature

--- src/components/YourNotes/VerseNotes.tsx
+++ src/components/YourNotes/VerseNotes.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { getAllNotesKeys, useAppSelector } from "@/store";
+import { dbFuncs } from "@/util/db";
 import VerseComponent from "./VerseComponent";
 
 const VerseNotes = () => {
@@ -10,16 +12,52 @@ const VerseNotes = () => {
   return (
     <>
       {myNotes.length ? (
-        <>
-          {myNotes.map((key) => (
-            <VerseComponent verseKey={key} key={key} />
-          ))}
-        </>
+        <NotesList notesKeys={myNotes} />
       ) : (
         <div className="fs-4 text-center">
           <div>{t("no_notes")}</div>
         </div>
       )}
+    </>
+  );
+};
+
+interface NotesListProps {
+  notesKeys: string[];
+}
+
+const NotesList = ({ notesKeys }: NotesListProps) => {
+  const [loadingNotes, setLoadingNotes] = useState(false);
+
+  const notesBackup = () => {
+    if (loadingNotes) return;
+
+    setLoadingNotes(true);
+    dbFuncs.loadNotes().then((allNotes) => {
+      const blob = new Blob([JSON.stringify(allNotes)], {
+        type: "application/json;charset=utf-8",
+      });
+      const downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = "notesBackup.json";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      URL.revokeObjectURL(downloadLink.href);
+      document.body.removeChild(downloadLink);
+      setLoadingNotes(false);
+    });
+  };
+
+  return (
+    <>
+      <div className="text-center p-2">
+        <button className="btn btn-success" onClick={notesBackup}>
+          Download notes
+        </button>
+      </div>
+      {notesKeys.map((key) => (
+        <VerseComponent verseKey={key} key={key} />
+      ))}
     </>
   );
 };
