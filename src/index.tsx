c15429265f206cdf08d21e97dc28e5bfc016519e
EnlightenCode <arabicwriter09@gmail.com>
2024-01-25 17:14:59 +0100

    refactor p4

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import store from "./store";
 import { Provider } from "react-redux";
-
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import App from "./App";
 
 import "./i18n";
-import DataLoader from "./components/DataLoader";
+
+import store from "@/store";
+import App from "./App";
+
+import Layout from "@/components/Layout/Layout";
+import DataLoader from "@/components/Custom/DataLoader";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
