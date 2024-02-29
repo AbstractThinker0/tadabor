7de6004073b7f92a5319f72f88b2c3f6ed58eb1a
EnlightenCode <arabicwriter09@gmail.com>
2024-02-29 10:46:08 +0100

    Add search options to Searcher2

--- src/pages/Searcher2.tsx
+++ src/pages/Searcher2.tsx
@@ -1,4 +1,5 @@
-import { Fragment, useState, useTransition } from "react";
+import { Fragment, useEffect, useState, useTransition } from "react";
+import { useTranslation } from "react-i18next";
 
 import useQuran from "@/context/useQuran";
 import { verseMatchResult } from "@/types";
@@ -15,9 +16,35 @@ const Searcher2 = () => {
   const [isPending, startTransition] = useTransition();
   const [stateVerses, setStateVerses] = useState<verseMatchResult[]>([]);
   const [itemsCount, setItemsCount] = useState(80);
+  const [searchIdentical, setSearchIdentical] = useState(false);
+  const [searchDiacritics, setSearchDiacritics] = useState(false);
+  const { i18n, t } = useTranslation();
 
   const searchStringHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchString(event.target.value);
+  };
+
+  function handleScroll(event: React.UIEvent<HTMLDivElement>) {
+    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
+    // Reached the bottom, ( the +10 is needed since the scrollHeight - scrollTop doesn't seem to go to the very bottom for some reason )
+    if (scrollHeight - scrollTop <= clientHeight + 10) {
+      setItemsCount((state) => state + 15);
+    }
+  }
+
+  const onChangeSearchIdentical = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchIdentical(event.target.checked);
+  };
+
+  const onChangeSearchDiacritics = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchDiacritics(event.target.checked);
+  };
+
+  useEffect(() => {
     startTransition(() => {
       const getSearchResult = () => {
         const matchVerses: verseMatchResult[] = [];
@@ -25,7 +52,12 @@ const Searcher2 = () => {
         const quranText = quranService.absoluteQuran;
 
         for (const verse of quranText) {
-          const result = searchVerse(verse, event.target.value, false, false);
+          const result = searchVerse(
+            verse,
+            searchString,
+            searchIdentical,
+            searchDiacritics
+          );
 
           if (result) {
             matchVerses.push(result);
@@ -37,15 +69,7 @@ const Searcher2 = () => {
 
       setStateVerses(getSearchResult());
     });
-  };
-
-  function handleScroll(event: React.UIEvent<HTMLDivElement>) {
-    const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
-    // Reached the bottom, ( the +10 is needed since the scrollHeight - scrollTop doesn't seem to go to the very bottom for some reason )
-    if (scrollHeight - scrollTop <= clientHeight + 10) {
-      setItemsCount((state) => state + 15);
-    }
-  }
+  }, [searchString, searchIdentical, searchDiacritics]);
 
   return (
     <div className="searcher2">
@@ -62,27 +86,70 @@ const Searcher2 = () => {
             dir="rtl"
           />
         </div>
+        <div className="d-flex gap-1">
+          <span className="fw-bold">{t("search_options")}</span>
+          <div
+            className={`form-check   ${
+              i18n.resolvedLanguage === "ar" && "form-check-reverse"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              checked={searchIdentical}
+              onChange={onChangeSearchIdentical}
+              value=""
+              id="CheckIdentical"
+            />
+            <label className="form-check-label" htmlFor="CheckIdentical">
+              {t("search_identical")}
+            </label>
+          </div>
+          <div
+            className={`form-check   ${
+              i18n.resolvedLanguage === "ar" && "form-check-reverse"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              checked={searchDiacritics}
+              onChange={onChangeSearchDiacritics}
+              value=""
+              id="CheckDiacritics"
+            />
+            <label className="form-check-label" htmlFor="CheckDiacritics">
+              {t("search_diacritics")}
+            </label>
+          </div>
+        </div>
       </div>
-      <div className="searcher2-list " dir="rtl" onScroll={handleScroll}>
-        {isPending ? (
-          <LoadingSpinner />
-        ) : (
-          stateVerses.slice(0, itemsCount).map((verseMatch) => (
-            <div
-              className="searcher2-list-verse border-bottom"
-              key={verseMatch.key}
-            >
-              <div>
-                <HighlightedText verse={verseMatch} />
-                <span>{` (${quranService.getChapterName(verseMatch.suraid)}:${
-                  verseMatch.verseid
-                })`}</span>
-                <ExpandButton identifier={verseMatch.key} />
+      <div className="searcher2-display">
+        <div
+          className="searcher2-display-list "
+          dir="rtl"
+          onScroll={handleScroll}
+        >
+          {isPending ? (
+            <LoadingSpinner />
+          ) : (
+            stateVerses.slice(0, itemsCount).map((verseMatch) => (
+              <div
+                className="searcher2-display-list-verse border-bottom"
+                key={verseMatch.key}
+              >
+                <div>
+                  <HighlightedText verse={verseMatch} />
+                  <span>{` (${quranService.getChapterName(verseMatch.suraid)}:${
+                    verseMatch.verseid
+                  })`}</span>
+                  <ExpandButton identifier={verseMatch.key} />
+                </div>
+                <NoteText verseKey={verseMatch.key} />
               </div>
-              <NoteText verseKey={verseMatch.key} />
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
