3565eff19cded1dc466f80188fd898a9f300d42b
EnlightenCode <arabicwriter09@gmail.com>
2024-05-16 18:03:04 +0100

    small cleanup

--- src/components/Coloring/consts.ts
+++ src/components/Coloring/consts.ts
@@ -1,16 +1,3 @@
-import { selectedChaptersType, verseProps } from "@/types";
-
-export interface stateProps {
-  currentChapter: number;
-  colorsList: coloredProps;
-  selectedColors: coloredProps;
-  coloredVerses: coloredProps;
-  currentVerse: verseProps | null;
-  currentColor: colorProps | null;
-  selectedChapters: selectedChaptersType;
-  scrollKey: string;
-}
-
 export interface colorProps {
   colorID: string;
   colorCode: string;
