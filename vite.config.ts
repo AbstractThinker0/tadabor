43a0191b1523dea1313eda7d0f145c0aa8c151e2
EnlightenCode <arabicwriter09@gmail.com>
2025-03-03 23:12:21 +0100

    Update chakra version

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     outDir: "build",
   },
