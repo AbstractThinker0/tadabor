46fdca91974ce3a4ce71759c9b21e6f32f23e942
AbstractThinker0 <arabicwriter09@gmail.com>
2025-03-25 23:08:32 +0100

    ref ui p19

---
+++ theme/slot-recipes/collapsible.ts
@@ -0,0 +1,22 @@
+import { defineSlotRecipe } from "@chakra-ui/react";
+
+export const collapsibleSlotRecipe = defineSlotRecipe({
+  slots: ["root", "trigger", "content"],
+  className: "chakra-collapsible",
+  base: {
+    root: {
+      direction: "inherit",
+    },
+    content: {
+      overflow: "hidden",
+      _open: {
+        animationName: "expand-height, fade-in",
+        animationDuration: "moderate",
+      },
+      _closed: {
+        animationName: "collapse-height, fade-out",
+        animationDuration: "moderate",
+      },
+    },
+  },
+});
