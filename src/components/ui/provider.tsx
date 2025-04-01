97fc3d97940a01713ebeea690687c0d808c99e5b
AbstractThinker0 <arabicwriter09@gmail.com>
2025-04-01 14:08:58 +0100

    move theme folder

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "theme/index";
+import { system } from "@/theme/index";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
