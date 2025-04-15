12d3f07ecc52b9b43a02e73e01d0fa5a3010730c
AbstractThinker0 <arabicwriter09@gmail.com>
2025-04-15 22:56:30 +0100

    ref p35

---
+++ src/components/Generic/Sidebar.tsx
@@ -0,0 +1,54 @@
+import {
+  CloseButton,
+  Collapsible,
+  Drawer,
+  Portal,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+interface SidebarProps {
+  isOpen: boolean;
+  setOpenState: (state: boolean) => void;
+  children: React.ReactNode;
+}
+
+const Sidebar = ({ isOpen, setOpenState, children }: SidebarProps) => {
+  // Use Chakra's breakpoint to determine if it's mobile
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const handleChangeOpen = (state: boolean) => {
+    setOpenState(state);
+  };
+
+  return (
+    <>
+      {isMobile ? (
+        <Drawer.Root
+          open={isOpen}
+          onOpenChange={(e) => handleChangeOpen(e.open)}
+          placement={"start"}
+        >
+          <Portal>
+            <Drawer.Backdrop />
+            <Drawer.Positioner>
+              <Drawer.Content>
+                <Drawer.Body px={1} py={0}>
+                  {children}
+                </Drawer.Body>
+                <Drawer.CloseTrigger asChild>
+                  <CloseButton size="sm" />
+                </Drawer.CloseTrigger>
+              </Drawer.Content>
+            </Drawer.Positioner>
+          </Portal>
+        </Drawer.Root>
+      ) : (
+        <Collapsible.Root open={isOpen} lazyMount unmountOnExit>
+          <Collapsible.Content>{children}</Collapsible.Content>
+        </Collapsible.Root>
+      )}
+    </>
+  );
+};
+
+export { Sidebar };
--- src/pages/QuranBrowser.tsx
+++ src/pages/QuranBrowser.tsx
@@ -1,16 +1,12 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import { qbPageActions } from "@/store/slices/pages/quranBrowser";
-import {
-  CloseButton,
-  Collapsible,
-  Drawer,
-  Flex,
-  Portal,
-  useBreakpointValue,
-} from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import SearchPanel from "@/components/Pages/QuranBrowser/SearchPanel";
 import DisplayPanel from "@/components/Pages/QuranBrowser/DisplayPanel";
+
+import { Sidebar } from "@/components/Generic/SideBar";
+
 import { usePageNav } from "@/hooks/usePageNav";
 
 function QuranBrowser() {
@@ -27,36 +23,16 @@ function QuranBrowser() {
 
   // Use Chakra's breakpoint to determine if it's mobile
   const isMobile = useBreakpointValue({ base: true, md: false });
+  const isOpen = isMobile ? showSearchPanelMobile : showSearchPanel;
+  const setOpenState = (state: boolean) => {
+    dispatch(qbPageActions.setSearchPanel(state));
+  };
 
   return (
     <Flex bgColor="brand.bg" overflow="hidden" maxH="100%" h="100%">
-      {isMobile ? (
-        <Drawer.Root
-          open={showSearchPanelMobile}
-          onOpenChange={(e) => dispatch(qbPageActions.setSearchPanel(e.open))}
-          placement={"start"}
-        >
-          <Portal>
-            <Drawer.Backdrop />
-            <Drawer.Positioner>
-              <Drawer.Content>
-                <Drawer.Body px={1} py={0}>
-                  <SearchPanel />
-                </Drawer.Body>
-                <Drawer.CloseTrigger asChild>
-                  <CloseButton size="sm" />
-                </Drawer.CloseTrigger>
-              </Drawer.Content>
-            </Drawer.Positioner>
-          </Portal>
-        </Drawer.Root>
-      ) : (
-        <Collapsible.Root open={showSearchPanel} lazyMount unmountOnExit>
-          <Collapsible.Content>
-            <SearchPanel />
-          </Collapsible.Content>
-        </Collapsible.Root>
-      )}
+      <Sidebar isOpen={isOpen} setOpenState={setOpenState}>
+        <SearchPanel />
+      </Sidebar>
       <DisplayPanel />
     </Flex>
   );
