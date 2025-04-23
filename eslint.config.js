78899915820e67d90d7687594b162e25572d6784
AbstractThinker0 <arabicwriter09@gmail.com>
2025-04-23 15:20:43 +0100

    refactor: update eslint and babel-plugin-react-compiler versions

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import ts from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactCompiler from "eslint-plugin-react-compiler";
 import importPlugin from "eslint-plugin-import";
 import reactRefresh from "eslint-plugin-react-refresh";
 import react from "eslint-plugin-react";
@@ -26,7 +25,6 @@ export default [
     plugins: {
       "@typescript-eslint": ts,
       "react-hooks": reactHooks,
-      "react-compiler": reactCompiler,
       import: importPlugin,
       "react-refresh": reactRefresh,
       react,
@@ -47,7 +45,7 @@ export default [
         },
       ],
       "react/react-in-jsx-scope": "off",
-      "react-compiler/react-compiler": "error",
+      "react-hooks/react-compiler": "error",
       "react-refresh/only-export-components": "warn",
       "import/no-unresolved": [
         "error",
