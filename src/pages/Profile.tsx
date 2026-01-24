d65acff73637992ee1d53f92475db68a5abe335d
AbstractThinker0 <arabicwriter09@gmail.com>
2026-01-24 13:16:43 +0100

    feat: implement tabbed navigation for user profile with bio and update sections

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,249 @@
 import { useState } from "react";
-import { Flex, Button, Input, Box, Text, Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Input,
+  Box,
+  Text,
+  Link,
+  Tabs,
+  Heading,
+} from "@chakra-ui/react";
 import { useUserStore } from "@/store/global/userStore";
 import { PasswordInput } from "@/components/ui/password-input";
 import { usePageNav } from "@/hooks/usePageNav";
+import useScreenSize from "@/hooks/useScreenSize";
 import { NavLink } from "react-router";
 import { validator } from "@/util/validators";
 import { useAuth } from "@/hooks/useAuth";
 import { useTranslation } from "react-i18next";
 import { tryCatch } from "@/util/trycatch";
 
+import TextareaAutosize from "@/components/Note/TextareaAutosize";
+import { useUserUpdateBio } from "@/services/backend";
+import { toaster } from "@/components/ui/toaster";
+
+import { FaRegUser } from "react-icons/fa6";
+import { LiaUserEditSolid } from "react-icons/lia";
+import { PiDevicesLight } from "react-icons/pi";
+import { MdOutlineRestore } from "react-icons/md";
+import { MdOutlineAutoAwesome } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdCancel } from "react-icons/md";
+
 const Profile = () => {
   usePageNav("auth.profile");
 
+  const [currentTab, setCurrentTab] = useState("userBio");
+  const isSmallScreen = useScreenSize();
+
+  return (
+    <Flex flex={1} overflow={"hidden"}>
+      <Tabs.Root
+        value={currentTab}
+        onValueChange={(e) => setCurrentTab(e.value)}
+        orientation={isSmallScreen ? "horizontal" : "vertical"}
+        flex={1}
+        flexDirection={isSmallScreen ? "column" : "row"}
+      >
+        <Tabs.List
+          fontSize={"small"}
+          width={isSmallScreen ? "100%" : "220px"}
+          justifyContent={isSmallScreen ? "space-around" : "flex-start"}
+        >
+          <Tabs.Trigger
+            padding={1}
+            fontSize={"small"}
+            value="userBio"
+            gap={2}
+            title="Your bio"
+          >
+            <FaRegUser />
+            {!isSmallScreen && "Your bio"}
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            padding={1}
+            fontSize={"small"}
+            value="updateProfile"
+            gap={2}
+            title="Update Profile"
+          >
+            <LiaUserEditSolid />
+            {!isSmallScreen && "Update Profile"}
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            padding={1}
+            fontSize={"small"}
+            value="Connected"
+            gap={2}
+            title="Connected devices"
+          >
+            <PiDevicesLight />
+            {!isSmallScreen && "Connected devices"}
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.ContentGroup>
+          <Tabs.Content value="userBio">
+            <UserBio />
+          </Tabs.Content>
+          <Tabs.Content
+            display={"flex"}
+            value="updateProfile"
+            paddingInlineStart={0}
+          >
+            <UpdateProfile />
+          </Tabs.Content>
+          <Tabs.Content value="Connected">
+            <Flex flex={1} justifyContent={"center"} alignItems={"center"}>
+              <Text fontSize={"lg"}>Coming soon..</Text>
+            </Flex>
+          </Tabs.Content>
+        </Tabs.ContentGroup>
+      </Tabs.Root>
+    </Flex>
+  );
+};
+
+const UserBio = () => {
+  const { t } = useTranslation();
+
+  const [editMode, setEditMode] = useState(false);
+
+  const userId = useUserStore((state) => state.id);
+  const avatarSeedOld = useUserStore((state) => state.avatarSeed);
+  const descriptionOld = useUserStore((state) => state.description);
+
+  const updateMetaProfile = useUserStore((state) => state.UpdateMetaProfile);
+
+  const updateBio = useUserUpdateBio();
+
+  const [description, setDescription] = useState(descriptionOld);
+  const [avatarSeed, setAvatarSeed] = useState(avatarSeedOld);
+
+  const onClickBioButton = (status: boolean) => {
+    setEditMode(status);
+
+    if (!status) {
+      setDescription(descriptionOld);
+    }
+  };
+
+  const onClickSave = async () => {
+    const { error } = await tryCatch(
+      updateBio.mutateAsync({ description, avatarSeed })
+    );
+
+    if (error) {
+      console.error("Failed to update bio:", error);
+      toaster.create({
+        description: t("ui.messages.save_failed"),
+        type: "error",
+      });
+    } else {
+      toaster.create({
+        description: t("ui.messages.save_success"),
+        type: "success",
+      });
+
+      updateMetaProfile(avatarSeed, description);
+      setEditMode(false);
+    }
+  };
+
+  return (
+    <Flex flexDirection={"column"} padding={4} gap={2}>
+      <Flex
+        flexDirection={"column"}
+        gap={4}
+        justifyContent={"center"}
+        alignItems={"center"}
+        paddingTop={5}
+      >
+        <Flex
+          borderStyle={"solid"}
+          borderWidth={"1px"}
+          borderRadius={"full"}
+          borderColor={"white"}
+          padding={4}
+          ring={"2px"}
+          ringColor={"white"}
+          shadow={"sm"}
+        >
+          <img
+            height={"70px"}
+            width={"70px"}
+            src={`https://api.dicebear.com/9.x/identicon/svg?seed=${avatarSeed}`}
+            alt="avatar"
+          />
+        </Flex>
+        <Flex gap={2}>
+          <Button
+            borderRadius={"xl"}
+            onClick={() => setAvatarSeed(userId.toString())}
+            variant={"outline"}
+          >
+            <MdOutlineRestore /> Restore default
+          </Button>
+          <Button
+            borderRadius={"xl"}
+            onClick={() => setAvatarSeed(Date.now().toString())}
+            colorPalette={"yellow"}
+          >
+            <MdOutlineAutoAwesome />
+            Random Avatar
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex
+        flexDirection={"column"}
+        gap={2}
+        justifyContent={"center"}
+        alignItems={"center"}
+        paddingTop={8}
+      >
+        <Heading size={"2xl"}>Bio</Heading>
+        <Flex flex={1} width={"100%"} px={5}>
+          <TextareaAutosize
+            dir="auto"
+            disabled={!editMode}
+            _disabled={{ cursor: "default" }}
+            resize={editMode ? "block" : "none"}
+            width={"100%"}
+            outline={"none"}
+            placeholder="Your about me.."
+            textAlign={"center"}
+            value={description}
+            fontSize={"medium"}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Flex>
+        <Button variant={"ghost"} onClick={() => onClickBioButton(!editMode)}>
+          {editMode ? (
+            <>
+              <MdCancel /> Cancel edit
+            </>
+          ) : (
+            <>
+              <MdEdit /> Edit Bio
+            </>
+          )}
+        </Button>
+      </Flex>
+      <Box textAlign={"center"}>
+        <Button
+          borderRadius={"3xl"}
+          shadow={"md"}
+          onClick={onClickSave}
+          px={10}
+        >
+          Save Changes
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+const UpdateProfile = () => {
   const { t } = useTranslation();
 
   const { updateProfile, updateProfilePassword } = useAuth();
@@ -47,7 +279,7 @@ const Profile = () => {
     setValidationError("");
 
     if (sameEmail && sameUsername) {
-      setValidationError("Idential to old email and username");
+      setValidationError("Identical to old email and username");
       return;
     }
 
@@ -133,16 +365,11 @@ const Profile = () => {
       flex={1}
       bg="gray.subtle"
       justifyItems={"center"}
-      alignItems={"center"}
       flexDirection="column"
       p={4}
+      fontSize={"small"}
     >
-      <Flex
-        flexDirection="column"
-        width={"50vw"}
-        mdDown={{ width: "90vw" }}
-        mb={4}
-      >
+      <Flex flexDirection="column" mb={4}>
         <Text>{t("auth.username")}</Text>
         <Input
           direction={"ltr"}
@@ -183,7 +410,7 @@ const Profile = () => {
           </Button>
         </Box>
       </Flex>
-      <Flex flexDirection="column" width={"50vw"} mdDown={{ width: "90vw" }}>
+      <Flex flexDirection="column">
         <Flex justifyContent={"space-between"}>
           <Text>{t("auth.oldPassword")}</Text>
           <Link colorPalette={"teal"} asChild>
